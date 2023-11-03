b0f28ac2d437dd7fcc861d4f9b9ccaffea445111
Samily <samilycv@gmail.com>
2023-11-03 15:12:56 +0530

    portfolio

---
+++ src/components/About/About.js
@@ -0,0 +1,133 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../../styles/About/About.scss";
+
+const About = () => {
+
+  return (
+    <>
+      <div className="about" id="about">
+        <div className="about__container">
+          <div className="about__header" id="about__heading">
+            <h1 data-aos="fade-down" data-aos-delay="0" data-aos-offset="20">
+              <span
+                data-aos="fade-down"
+                data-aos-delay="0"
+                data-aos-offset="200"
+              >
+                A
+              </span>
+              <span
+                data-aos="fade-down"
+                data-aos-delay="200"
+                data-aos-offset="200"
+              >
+                B
+              </span>
+              <span
+                data-aos="fade-down"
+                data-aos-delay="400"
+                data-aos-offset="200"
+              >
+                O
+              </span>
+              <span
+                data-aos="fade-down"
+                data-aos-delay="600"
+                data-aos-offset="200"
+              >
+                U
+              </span>
+              <span
+                data-aos="fade-down"
+                data-aos-delay="800"
+                data-aos-offset="200"
+              >
+                T
+              </span>
+              <span
+                data-aos="fade-down"
+                data-aos-delay="1000"
+                data-aos-offset="200"
+              >
+                .
+              </span>
+            </h1>
+            <p></p>
+          </div>
+
+          <div className="about__details-container">
+            <div
+              className="about__details"
+              data-aos="fade-down"
+              data-aos-offset="0"
+              data-aos-delay="0"
+              data-aos-once="true"
+            >
+              <Link to="/about/intro">
+                <h1>
+                  0<span>1</span>
+                </h1>
+                <h2>
+                  H<span>ello,</span>
+                </h2>
+                <p>
+                  I am <span>Samily C Vijayan.</span> <br />Passoinate
+                  Mearn Stack Developer as fresher.
+                </p>
+              </Link>
+            </div>
+            
+            <div
+              className="about__details"
+              data-aos="fade-down"
+              data-aos-offset="50"
+              data-aos-delay="500"
+              data-aos-once="true"
+            >
+              <Link to="/about/internships">
+                <h1>
+                  0<span>2</span>
+                </h1>
+                <h2>
+                  I<span>nternship</span>
+                </h2>
+                <p>
+                  <span>Interned at -</span> <br />
+                </p>
+                <ul>
+                  <li>
+                    <span>Luminar Technolab, Kochi</span> - MEARN Stack Developer{" "}
+                  </li>
+                  
+                </ul>
+              </Link>
+            </div>
+            <div
+              className="about__details"
+              data-aos="fade-down"
+              data-aos-offset="-180"
+              data-aos-delay="700"
+              data-aos-once="true"
+            >
+              <Link to="/about/graduation">
+                <h1>
+                  0<span>3</span>
+                </h1>
+                <h2>
+                  M<span>CA</span>
+                </h2>
+                <p>
+                  <span>Vidya Academy Of Science & Technology, Thrissur</span>
+                </p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const config = {
+  portfolio: {
+    portfolioLogo: 'Portfolio',
+    projectCard: {
+      cards: [
+        {
+          heading: 'Counter App',
+          description:
+            'Project description. Created a counter app using React.It includes Increment button,Decrement button and a Reset button',
+          animationDelay: '300',
+          buttonLink: 'https://samilycounterapp.netlify.app',
+          buttonText: 'CHECK OUT',
+        },
+        {
+          heading: 'Discount Calculator',
+          description:
+            'Project description. Created a discount calculator for calculating the amount with discount rate using React.',
+          animationDelay: '500',
+          buttonLink: 'https://samilydiscount-calculator.netlify.app',
+          buttonText: 'CHECK OUT',
+        },
+        {
+          heading: 'Weather App',
+          description: 'Project description. Created a weather app for find the weather condition in different places using Angular.',
+          animationDelay: '700',
+          buttonLink: 'https://samilycv.github.io/weatherapp/',
+          buttonText: 'CHECK OUT',
+        },
+        {
+          heading: 'Budget Calculator',
+          description:
+            'Project description. Created a budget calculator for calculating the budget using HTML, CSS ,Bootstrap and Javascript.',
+          animationDelay: '900',
+          buttonLink: 'https://samilycv.github.io/budgetcalculator/',
+          buttonText: 'CHECK OUT',
+        },
+        {
+          heading: 'Luminar Technolab Website',
+          description: 
+          'Project description. Created a simple Luminar Technolab Website usin HTML,CSS and Bootstrap.',
+          animationDelay: '1100',
+          buttonLink: 'https://samilycv.github.io/luminar/',
+          buttonText: 'CHECK OUT',
+        },
+      ],
+    },
+    internships: {
+      internship: [
+        {
+          companyName: 'Luminar Technolab',
+          role: 'MEARN Stack Developer',
+          introText: '',
+          duration: '10/04/2023 - Present',
+          keyPointOne: 'Built multiple websites using Angular,NodeJS,MongoDB',
+          keyPointTwo:'Built multiple websites using Reat,NodeJS,MongoDB',
+          keyPointThree:'Built websites using HTML And CSS',
+          keyPointFour:'Built wesites using Bootstrap',
+        },
+       
+      ],
+    },
+  },
+};
+
+export default config;
